926ceff5b2025003d961ea4098c0b6e4eac19c6b
Sachin Karki <karkisachin455@gmail.com>
2023-04-08 22:18:52 +0545

    adding more

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import renfolder
 import list
+import crt_file
+import del_file
+import del_folder
 
 renfolder.renamefolder()
 list.list_dir()
+crt_file.create_file()
+del_file.delete()
+del_folder.delete()
 
